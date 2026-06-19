8397d0c3292c9922f5897f07be72cb54393aab82
M GirishKumar <mgirishkumar67890@gmail.com>
2026-06-19 12:59:44 +0530

    New Class added

---
+++ src/freefiremax/GitDemo.java
@@ -0,0 +1,16 @@
+package freefiremax;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+
+public class GitDemo {
+	WebDriver driver;
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
